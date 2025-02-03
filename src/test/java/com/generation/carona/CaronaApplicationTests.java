5fb60bee2bdba7e61053eb0436773bdbad80e8c4
User <raquelmorabito@hotmail.com>
2025-02-03 01:55:32 -0300

    configurações iniciais

---
+++ src/test/java/com/generation/carona/CaronaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.carona;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaronaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
